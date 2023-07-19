60a521d7559449c2639692a75e50a2b6a7a67259
drb <darlanvcon@gmail.com>
2023-07-19 10:36:51 -0300

    versão inicial

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+var setaesq = window.document.getElementById("setaesq")
+var leo = window.document.getElementById("leo")
+var sama = window.document.getElementById("sama")
+var brun = window.document.getElementById("brun")
+var setadir = window.document.getElementById("setadir")
+
+
+function roldir () {
+leo.style = "display:none"
+brun.style = "display:flex" 
+setadir.style = "display:flex; margin-top: 55px"
+setaesq.style = "display:none"
+
+}
+
+function rolesq () {
+    leo.style = "display:flex"
+    brun.style = "display:none" 
+    setadir.style = "display:none; margin-top: 55px"
+    setaesq.style = "display:flex"
+    
+    }
