9b973acc163bc8d583de54b8476713f2a3839370
julio cesar <jcmcf2012@gmail.com>
2023-09-28 12:13:01 -0300

    fix: add a linked list functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
-var leo = window.document.getElementById("leo");
-var sama = window.document.getElementById("sama");
-var brun = window.document.getElementById("brun");
+const leo = window.document.getElementById("leo");
+const sama = window.document.getElementById("sama");
+const brun = window.document.getElementById("brun");
+let actualPosition = 0; // 1 // 2
+let users = [leo, sama, brun];
 
 function roldir() {
-  leo.style = "display:none";
-  brun.style = "display:flex";
+  // Atualiza posição atual
+  users[actualPosition].style = "display:none";
+  // Atualiza numero do indice para controlar os usuários
+  if (actualPosition == users.length - 1) {
+    actualPosition = 0;
+  } else {
+    actualPosition++;
+  }
+  // Atualiza próximo usuário
+  users[actualPosition].style = "display:flex";
 }
 
 function rolesq() {
-  leo.style = "display:flex";
-  brun.style = "display:none";
+  // Atualiza posição atual
+  users[actualPosition].style = "display:none";
+  // Atualiza numero do indice para controlar os usuários
+  if (actualPosition == 0) {
+    actualPosition = users.length - 1;
+  } else {
+    actualPosition--;
+  }
+  // Atualiza próximo usuário
+  users[actualPosition].style = "display:flex";
 }
