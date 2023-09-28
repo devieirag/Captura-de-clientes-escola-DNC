567efb659321afb3ecc54ef6290ad96334152915
drb <darlanvcon@gmail.com>
2023-09-28 09:32:27 -0300

    carrossel a consertar

--- js/index.js
+++ js/index.js
@@ -5,20 +5,22 @@ var sama = window.document.getElementById("sama")
 var brun = window.document.getElementById("brun")
 var setadir = window.document.getElementById("setadir")
 
-
 function roldir () {
+
 leo.style = "display:none"
 brun.style = "display:flex" 
 sama.style = "display:none"
-setadir.style = "display:flex; margin-top: 20%"
+setadir.style = "display:flex"
 setaesq.style = "display:none"
 
 }
 
+
+
 function rolesq () {
-    leo.style = "display:flex"
-    brun.style = "display:none"
-    sama.style = "display:flex" 
+    leo.style = "display:none"
+    brun.style = "display:flex"
+    sama.style = "display:none" 
     setadir.style = "display:none;"
     setaesq.style = "display:flex"
     
